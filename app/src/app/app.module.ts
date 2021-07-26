c55cb3b1d3a3e8cdd0461cbf5922310cd827bd28
kusano shunsuke <kusano@thedott.io>
2021-07-26 10:26:15 +0900

    add mock up

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClickOutsideModule } from 'ng-click-outside';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialImportModule } from './material/mat-shared.module';
+import { DialogComponent } from './component/dialog/dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClickOutsideModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AngularMaterialImportModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
