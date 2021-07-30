27ac946f67d8ed5df8a6d72c07f61afb16aa07ff
中村洸 <hikaru.nakamura@thedott.io>
2021-07-30 11:52:13 +0900

    年齢の自動算出修正

--- app/src/app/nakamura/nakamura.component.ts
+++ app/src/app/nakamura/nakamura.component.ts
@@ -2,25 +2,27 @@ import { Component, ViewChild } from '@angular/core';
 import { DateAdapter, NativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { MatTable, MatTableModule } from '@angular/material/table';
+import * as moment from 'moment';
 
 
 export interface PersonInfo {
+  no: number;
   name: string;
-  birthday: string;
+  birthday: Date;
   age: number;
 }
 
 const PERSONAL_DATA: PersonInfo[] = [
-  {name: 'Hydrogen', birthday:'1996/9/7', age:22},
-  {name: 'Helium', birthday:'1996/9/7', age:22},
-  {name: 'Lithium', birthday:'1996/9/7', age:22},
-  {name: 'Beryllium', birthday:'1996/9/7', age:22},
-  {name: 'Boron', birthday:'1996/9/7', age:22},
-  {name: 'Carbon', birthday:'1996/9/7', age:22},
-  {name: 'Nitrogen', birthday:'1996/9/7', age:22},
-  {name: 'Oxygen', birthday:'1996/9/7', age:22},
-  {name: 'Fluorine', birthday:'1996/9/7', age:22},
-  {name: 'Neon', birthday:'1996/9/7', age:22},
+  {no:0, name: 'test', birthday: new Date('1996/9/3'), age:25},
+  {no:1, name: 'Hydrogen', birthday: new Date('1996/9/7'), age:25},
+  {no:2, name: 'Helium', birthday: new Date('1996/11/7'), age:25},
+  {no:3, name: 'Lithium', birthday: new Date('1996/12/7'), age:25},
+  {no:4, name: 'Beryllium', birthday: new Date('1996/9/11'), age:25},
+  {no:5, name: 'Boron', birthday: new Date('1996/9/7'), age:25},
+  {no:6, name: 'Carbon', birthday: new Date('1996/9/7'), age:25},
+  {no:7, name: 'Carbon', birthday: new Date('1996/8/7'), age:25},
+  {no:8, name: 'Carbon', birthday: new Date('1996/9/17'), age:25},
+  {no:9, name: 'Carbon', birthday: new Date('1996/9/27'), age:25}
 ];
 
 @Component({
@@ -29,7 +31,7 @@ const PERSONAL_DATA: PersonInfo[] = [
   styleUrls: ['./nakamura.component.scss']
 })
 export class NakamuraComponent {
-  person: PersonInfo =  {name: '', birthday: '', age:0 };
+  person: PersonInfo =  {no:PERSONAL_DATA.length,name: '', birthday: new Date(), age:0 };
   title = 'TestApp';
   displayedColumns: string[] = ['name','birthday','age'];
   dataSource = PERSONAL_DATA;
@@ -40,22 +42,22 @@ export class NakamuraComponent {
   }
 
   changeDate(event: MatDatepickerInputEvent<Date>) {
-    const today = new Date;
-    const y1 = today.getFullYear().toString().padStart(4, '0');
-    const m1 = (today.getMonth() + 1).toString().padStart(2, '0');
-    const d1 = today.getDate().toString().padStart(2, '0');
-
-    const birthday: Date = event.value == null ? new Date : event.value;
-    const y2 = birthday.getFullYear().toString().padStart(4, '0');
-    const m2 = (birthday.getMonth() + 1).toString().padStart(2, '0');
-    const d2 = birthday.getDate().toString().padStart(2, '0');
-
-    this.person.age = Math.floor((Number(y1 + m1 + d1) - Number(y2 + m2 + d2)) / 10000);
-    this.person.birthday = birthday.toLocaleDateString();
+    const currentDate = moment();
+    const birthday = moment(event.value);
+    this.person.age = currentDate.diff(birthday, 'year');
+    this.person.birthday = birthday.toDate();
   }
-  
+
   addPerson(){
     this.dataSource.push(this.person);
     this.table?.renderRows();
+    this.initPerson();
+  }
+
+  initPerson(){
+    this.person.no = this.dataSource.length;
+    this.person.name = '';
+    this.person.birthday = new Date();
+    this.person.age = 0;
   }
 }
