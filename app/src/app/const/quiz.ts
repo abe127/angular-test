9d24138146a94a431cf3b88cce7d1bc38e91270c
中村洸 <hikaru.nakamura@thedott.io>
2021-10-28 16:09:52 +0900

    クイズに解説を追加

--- app/src/app/const/quiz.ts
+++ app/src/app/const/quiz.ts
@@ -6,6 +6,7 @@ export interface Choice {
 export interface Quiz {
   question: string;
   choices: Choice[]
+  explanation: string;
 };
 
 export const QUIZ_DATA: Quiz[] = [
@@ -28,7 +29,8 @@ export const QUIZ_DATA: Quiz[] = [
         statement: '選択肢4',
         isAnswer: false
       }
-    ]
+    ],
+    explanation: '説明文'
   },
   {
     question: '1 + 1 = ?',
@@ -49,7 +51,8 @@ export const QUIZ_DATA: Quiz[] = [
         statement: '4',
         isAnswer: false
       }
-    ]
+    ],
+    explanation: 'https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1011667290'
   },
   {
     question: '日本の首都は？',
@@ -70,6 +73,7 @@ export const QUIZ_DATA: Quiz[] = [
         statement: '福岡',
         isAnswer: false
       }
-    ]
+    ],
+    explanation: '現在、日本の「首都」は、一般的に東京都と認識されている。日本の現行法令には「首都圏」の定義は存在するが（首都圏整備法）「首都」についての定義はなく、また過去においても「首都」という明治以前には一般的でなかった語と伝統的な用語である「都」「京」との関係について明確にされたことがなく、「日本の首都」という語そのものについて議論がある。'
   }
 ];
